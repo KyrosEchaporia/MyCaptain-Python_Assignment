9930d1285f7bdd133ad19aef0d85d532a609fb90
Kyros Echaporia <124595523+KyrosEchaporia@users.noreply.github.com>
2023-02-25 18:30:02 +0530

    Add files via upload

---
+++ assignment1-area of circle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 18:03:01 2023
+
+@author: Kyros Echaporia
+"""
+from math import pi
+
+rad=float(input("Input the radius of a circle:"))
+pi_value=pi
+area=pi*rad**2
+print("The area of the circle with radius",rad,"is:",area)
---
+++ assignment1-extension.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 17 18:16:29 2023
+
+@author: Kyros Echaporia
+"""
+
+filename=input("Input the filename: ")
+extension=filename.split('.')
+print("The extension of the file is: ",extension[1])
